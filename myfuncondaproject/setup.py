093b300946f1581d47d643b00d59cad43a5b74fb
Liam Pieri <liamwazherealso@gmail.com>
2022-09-13 12:11:51 -0400

    Add minimal package

---
+++ myfuncondaproject/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='myfuncondaproject',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Sandbox repo for conda packaging',
+    author='Liam Pieri',
+    license='',
+)
